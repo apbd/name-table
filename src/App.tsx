39783747d356611d79759ea46cb619a05cb7bdca
apbd <60779371+apbd@users.noreply.github.com>
2022-02-03 14:29:20 +0200

    Added person adding with formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,73 @@
-import React, { useState } from 'react';
-import './assets/App.css';
-import PersonCard from './components/PersonCard';
+import React, { useState } from 'react'
+import './assets/App.css'
+import PersonCard from './components/PersonCard'
+import { Formik, Field, Form, FormikHelpers } from 'formik'
 
 interface Person {
-  firstName: string, lastName: string, age: number
+	firstName: string
+	lastName: string
+	age: number
 }
-
 
 function App() {
-  
-  const [people, setPeople] = useState<Person[]>([{firstName: "John", lastName: "Doe", age: 30},{firstName: "Matti", lastName: "Meikäläinen", age: 20}])
-  
-  
+	const [people, setPeople] = useState<Person[]>([
+		{ firstName: 'John', lastName: 'Doe', age: 30 },
+		{ firstName: 'Matti', lastName: 'Meikäläinen', age: 20 },
+	])
 
-  const handleSubmit = (event: any) => { 
-    event.preventDefault();
-    console.log(event)
-    // setPeople([...people, { firstName: }] )
-  }
-  
-  return (
-    <div className="App">
-      <h1>People:</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" />
-        <input type="submit" />
-      </form>
 
-    {  people.map((person, index) => {
-      return <PersonCard key={index} firstName={person.firstName} lastName={person.lastName} age={person.age} />
-      })}
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>People:</h1>
+			<Formik
+				initialValues={{
+					firstName: '',
+					lastName: '',
+					age: 0,
+				}}
+				onSubmit={(
+					values: Person,
+					{ setSubmitting }: FormikHelpers<Person>
+				) => {
+					setTimeout(() => {
+						// console.log(JSON.stringify(values, null, 2))
+		setPeople([...people, values] )
+
+						setSubmitting(false)
+					}, 500)
+				}}
+			>
+				<Form>
+					<label htmlFor='firstName'>First Name</label>
+					<Field id='firstName' name='firstName' placeholder='John' />
+
+					<label htmlFor='lastName'>Last Name</label>
+					<Field id='lastName' name='lastName' placeholder='Doe' />
+
+					<label htmlFor='age'>Age</label>
+					<Field
+						id='age'
+						name='age'
+						placeholder='40'
+						type='number'
+					/>
+
+					<button type='submit'>Submit</button>
+				</Form>
+			</Formik>
+
+			{people.map((person, index) => {
+				return (
+					<PersonCard
+						key={index}
+						firstName={person.firstName}
+						lastName={person.lastName}
+						age={person.age}
+					/>
+				)
+			})}
+		</div>
+	)
 }
 
-export default App;
+export default App
